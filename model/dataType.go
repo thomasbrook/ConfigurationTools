9cb89d213ce6a95c5f05eac629241d9cb3b6c6fb
杜建平 <dujianping@uml-tech.com>
2020-12-04 17:18:32 +0800

    Update dataType.go

--- model/dataType.go
+++ model/dataType.go
@@ -10,9 +10,9 @@ const (
 	OtherValue   DataType = 4
 	EnumText     DataType = 5
 
-	//QZ_Mysql_driver = "root:l520m6ti@tcp(192.168.11.102:3306)/agfpiddb?charset=utf8"
-	QZ_Mysql_driver = "aguser:z3ZelaJc@tcp(172.16.1.27:3306)/agfpiddb?charset=utf8"
+	//QZ_Mysql_driver = "root:l520m6ti@tcp(localhost:3306)/agfpiddb?charset=utf8"
+	QZ_Mysql_driver = "aguser:z3ZelaJc@tcp(localhost:3306)/agfpiddb?charset=utf8"
 
-	//CONFIG_URL = "http://192.168.11.8/config.xml"
-	CONFIG_URL = "http://172.16.1.245:8000/config.xml"
+	//CONFIG_URL = "http://localhost/config.xml"
+	CONFIG_URL = "http://localhost/config.xml"
 )
